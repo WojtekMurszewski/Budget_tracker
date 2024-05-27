94c5e76228a1cd6237dbf9cb684d00bff7459448
s219079 <s219079@student.uek.krakow.pl>
2024-05-27 16:56:17 +0200

    Running out of ideas

--- budget_tracker/src/router/index.js
+++ budget_tracker/src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import MainLayout from '../components/MainLayout.vue';
 import HomePage from '../components/HomePage.vue';
 import NewTransaction from '../components/NewTransaction.vue';
@@ -7,40 +8,71 @@ import SettingsPage from '../components/SettingsPage.vue';
 import SignInSignUp from '../components/SignInSignUp.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // Changed to hash mode
   routes: [
     {
       path: '/',
+      name: 'Login Page',
+      component: SignInSignUp
+    },
+    {
+      path: '/main',
       component: MainLayout,
+      meta: { requiresAuth: true },
       children: [
         {
           path: '',
           name: 'HomePage',
-          component: HomePage
+          component: HomePage,
+          meta: { requiresAuth: true },
         },
         {
           path: '/new-transaction',
-          name: 'New Transaction',
-          component: NewTransaction
+          name: 'NewTransaction',
+          component: NewTransaction,
+          meta: { requiresAuth: true },
         },
         {
           path: '/profile-page',
-          name: 'Profile Page',
-          component: ProfilePage
+          name: 'ProfilePage',
+          component: ProfilePage,
+          meta: { requiresAuth: true },
         },
         {
           path: '/settings-page',
           name: 'Settings Page',
-          component: SettingsPage
-        },
-        {
-          path: '/signin-signup',
-          name: 'Login Page',
-          component: SignInSignUp
+          component: SettingsPage,
+          meta: { requiresAuth: true },
         }
       ]
     }
   ]
+});
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
+    } else {
+      alert("You don't have access!");
+      next("/");
+    }
+  } else {
+    next(); // Continue navigation
+  }
 });
 
 export default router;
--- budget_tracker/vue.config.js
+++ budget_tracker/vue.config.js
@@ -1,4 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/Project-PSM/' : '/',
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    }
+  },
+});
