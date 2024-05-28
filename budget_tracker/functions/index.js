3d8b5aa03aa4c566b5791fcd477e2e37b22367bb
WojtekMurszewski <wojtek.murszewski@gmail.com>
2024-05-28 20:05:01 +0200

    upgrade

--- budget_tracker/functions/index.js
+++ budget_tracker/functions/index.js
@@ -22,32 +22,3 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.dailyNotification = functions.pubsub.schedule('every day 20:00').timeZone('Europe/Warsaw').onRun(async (context) => {
-    const db = admin.firestore();
-    const usersRef = db.collection('userSettings');
-    const snapshot = await usersRef.where('notificationsEnabled', '==', true).get();
-    
-    if (snapshot.empty) {
-      console.log('No users found with notifications enabled.');
-      return null;
-    }
-  
-    const promises = [];
-    snapshot.forEach(doc => {
-      const userId = doc.id;
-      const payload = {
-        notification: {
-          title: 'Przypomnienie',
-          body: 'Pamiętaj, aby dodać wszystkie swoje transakcje dzisiaj.',
-          clickAction: 'FLUTTER_NOTIFICATION_CLICK', // Dostosuj, jeśli używasz aplikacji mobilnej
-        },
-      };
-  
-      // Send a notification to each user
-      promises.push(admin.messaging().sendToTopic(userId, payload));
-    });
-  
-    await Promise.all(promises);
-    console.log('Notifications sent successfully.');
-    return null;
-  });
